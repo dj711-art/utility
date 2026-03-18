91690a0f922344c93958bbc76a03a2cbd9e8f14f
Jash Debmalya <gkedeb@gmail.com>
2026-03-18 14:48:43 -0400

    NumUtil

--- src/main/java/org/own/util/NumUtil.java
+++ src/main/java/org/own/util/NumUtil.java
@@ -117,6 +117,16 @@ public class NumUtil {
     }
 
 
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 0) { return 0;  }
+        var max = nums[0];
+        var curr = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], curr+nums[i]);
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
 
 }
 
--- src/test/java/org/own/util/NumUtilTest.java
+++ src/test/java/org/own/util/NumUtilTest.java
@@ -10,23 +10,32 @@ class NumUtilTest {
 
     @Test
     void countResponseTimeRegressions() {
-        assertEquals(2,NumUtil.countResponseTimeRegressions(List.of(100,200,150,300)));
-        assertEquals(2,NumUtil.countResponseTimeRegressions(List.of(100,150,200,300)));
+        assertEquals(2, NumUtil.countResponseTimeRegressions(List.of(100, 200, 150, 300)));
+        assertEquals(2, NumUtil.countResponseTimeRegressions(List.of(100, 150, 200, 300)));
     }
 
     @Test
-    void removeElement(){
+    void removeElement() {
         assertEquals(2, NumUtil.removeElement(new int[]{3, 2, 2, 3}, 3));
     }
 
     @Test
-    void countSubMatrices(){
-        assertEquals(4,NumUtil.countSubMatrices(new int[][]{{7,6,3},{8,6,1}},18));
-        assertEquals(6,NumUtil.countSubMatrices(new int[][]{{7,2,9},{1,5,0},{2,6,6}},20));
+    void countSubMatrices() {
+        assertEquals(4, NumUtil.countSubMatrices(new int[][]{{7, 6, 3}, {8, 6, 1}}, 18));
+        assertEquals(6, NumUtil.countSubMatrices(new int[][]{{7, 2, 9}, {1, 5, 0}, {2, 6, 6}}, 20));
     }
 
     @Test
     void removeDuplicates() {
-        assertEquals(2,NumUtil.removeDuplicates(new int[]{1,1,2}));
+        assertEquals(2, NumUtil.removeDuplicates(new int[]{1, 1, 2}));
+    }
+
+    @Test
+    void maxSubArray() {
+        assertEquals(6, NumUtil.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        assertEquals(13, NumUtil.maxSubArray(new int[]{1, 2, 3, -4, 5, 6}));
+        assertEquals(-1, NumUtil.maxSubArray(new int[]{-1, -2, -3, -4, -5}));
+        assertEquals(15, NumUtil.maxSubArray(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(1, NumUtil.maxSubArray(new int[]{1, -1, 1, -1, 1}));
     }
 }
