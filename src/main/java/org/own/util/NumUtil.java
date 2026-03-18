c432190f6c76d56a827ae9d7e1a78533bbfec68c
Jash Debmalya <gkedeb@gmail.com>
2026-03-17 23:07:12 -0400

    NumUtil

--- src/main/java/org/own/util/NumUtil.java
+++ src/main/java/org/own/util/NumUtil.java
@@ -32,6 +32,68 @@ public class NumUtil {
         return count;
     }
 
+    /**
+     * From the given integer array nums, remove all occurrences of val from nums in-place.
+     * @param nums integer array.
+     * @param val integer value to be removed from the array.
+     * @return length of the new array.
+     */
+    public static int removeElement(int[] nums, int val) {
+
+        var size = nums.length;
+        var endIndex = size - 1;
+        var count = size;
+
+        for (var index = 0; index <= endIndex; index++) {
+            if (nums[index] == val) {
+                while (endIndex > index && nums[endIndex] == val) {
+                    endIndex--;
+                    count--;
+                }
+                if (index < endIndex) {
+                    nums[index] = nums[endIndex];
+                    endIndex--;
+                    count--;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * You are given a 0-indexed integer matrix grid and an integer k.
+     * <p>
+     * Return the number of that contain the top-left element of the grid, and have a sum less than or equal to k.
+     * @param grid 0-indexed integer matrix grid.
+     * @param k integer.
+     * @return the number of that contain the top-left element of the grid, and have a sum less than or equal to k.
+     */
+    public static int countSubMatrices(int[][] grid,int k){
+        var count = 0;
+        var rows = grid.length;
+        var cols = grid[0].length;
+        var prefixSum = new int[rows][cols];
+        for (var row = 0; row < rows; row++) {
+            for (var col = 0; col < cols; col++) {
+                prefixSum[row][col] = grid[row][col];
+                if (row > 0) {
+                    prefixSum[row][col] += prefixSum[row - 1][col];
+                }
+                if (col > 0) {
+                    prefixSum[row][col] += prefixSum[row][col - 1];
+                }
+                if (row > 0 && col > 0) {
+                    prefixSum[row][col] -= prefixSum[row - 1][col - 1];
+
+                }
+                if (prefixSum[row][col] <= k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
 }
 
--- src/test/java/org/own/util/NumUtilTest.java
+++ src/test/java/org/own/util/NumUtilTest.java
@@ -13,4 +13,15 @@ class NumUtilTest {
         assertEquals(2,NumUtil.countResponseTimeRegressions(List.of(100,200,150,300)));
         assertEquals(2,NumUtil.countResponseTimeRegressions(List.of(100,150,200,300)));
     }
+
+    @Test
+    void removeElement(){
+        assertEquals(2, NumUtil.removeElement(new int[]{3, 2, 2, 3}, 3));
+    }
+
+    @Test
+    void countSubMatrices(){
+        assertEquals(4,NumUtil.countSubMatrices(new int[][]{{7,6,3},{8,6,1}},18));
+        assertEquals(6,NumUtil.countSubMatrices(new int[][]{{7,2,9},{1,5,0},{2,6,6}},20));
+    }
 }
